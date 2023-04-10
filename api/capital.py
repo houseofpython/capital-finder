f5a0a4f675a050888c5e30d51ea7ed67e130fd7f
‘Diontre <diontre01@gmail.com>
2023-04-10 18:40:51 -0500

    part3

--- api/capital.py
+++ api/capital.py
@@ -4,12 +4,12 @@ import requests
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        s = self.path
-        url_components = parse.urlsplit(s)
-        print('url.components', url_components)
-        query_string_list = parse.parse_qsl(url_components.query)
-        print("query.string.list", query_string_list)
-        dic = dict(query_string_list)
+        # s = self.path
+        # url_components = parse.urlsplit(s)
+        # print('url.components', url_components)
+        # query_string_list = parse.parse_qsl(url_components.query)
+        # print("query.string.list", query_string_list)
+        # dic = dict(query_string_list)
         #
         # if capital in dic:
         #     url = f"https://restcountries.com/v3.1/capital/{capital}"
@@ -24,6 +24,7 @@ class handler(BaseHTTPRequestHandler):
         # else:
         #     message = "Give me a city name please"
         message = "test message"
+        print(message)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
